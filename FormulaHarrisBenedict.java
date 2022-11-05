ba878d37e9d7bbe3db07d611e5aaf3205d9bcb75
Noa <111797897+Nidadreamer@users.noreply.github.com>
2022-11-05 12:31:58 +0100

    Add files via upload

--- FormulaHarrisBenedict.java
+++ FormulaHarrisBenedict.java
@@ -9,47 +9,47 @@ public class FormulaHarrisBenedict {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Introduce el peso en kg:");
-            double peso = scanner.nextDouble();
+            double peso = sc.nextDouble();
             double pesof = peso * 10;
 
         System.out.println("              ");
         System.out.println("Introduce la altura en cm:");
-            double altura = scanner.nextDouble();
+            double altura = sc.nextDouble();
             double alturaf = altura * 6.25;
 
         System.out.println("               ");
         System.out.println("Introduce la edad en años:");
-            double years = scanner.nextDouble();
-            double yearsf = years * 5;
+            int years = sc.nextInt();
+            int yearsf = years * 5;
 
         System.out.println("               ");
         System.out.println("Asigna la actividad estimada, introduce el número que corresponda <<usa la COMA entre los números>>:");
         System.out.println(" ");
-        System.out.println("  *********************************************************************************************");
-        System.out.println("1,2 ---> Poco o ningún ejercicio" + "                       " + "1,375 -> Ejercicio ligero (1-3 días a la semana)"+
+        System.out.println("\33[35m  *********************************************************************************************");
+        System.out.println("\33[39;49m1,2 ---> Poco o ningún ejercicio" + "                       " + "1,375 -> Ejercicio ligero (1-3 días a la semana)"+
             "\n1,55 --> Ejercicio moderado (3-5 días a la semana)" + "     " + "1,725 -> Ejercicio fuerte (6-7 días a la semana)"+
             "\n1,9 ---> Ejercicio muy fuerte (dos veces al día, entrenamientos muy duros)");
-        System.out.println("  *********************************************************************************************");
-          double actividad = scanner.nextDouble();
+        System.out.println("\33[35m  *********************************************************************************************");
+          double actividad = sc.nextDouble();
 
         System.out.println("             ");
-        System.out.println("Introduce el sexo: \n1 si es hombres o 2 si es mujer");
-            int sexo = scanner.nextInt();
+        System.out.println("\33[39;49mIntroduce el sexo: \n1 si es hombres o 2 si es mujer");
+            int sexo = sc.nextInt();
 
             if (sexo == 1) {
                 double calculo = (pesof + alturaf - yearsf + 5) * actividad;
-                System.out.println("__________________________________________________________");
-                System.out.println("   Las calorías que necesita este hombre son: " + calculo );
-                System.out.println("__________________________________________________________");
+                System.out.println("\33[35m__________________________________________________________");
+                System.out.println("\33[39;49m   Las calorías que necesita este hombre son: " + calculo );
+                System.out.println("\33[35m__________________________________________________________");
             }
             else {
                 double calculo = (pesof + alturaf - yearsf - 161) * actividad;
-            System.out.println("__________________________________________________________");
-            System.out.println("   Las calorías que necesita esta mujer son:  " + calculo );
-            System.out.println("__________________________________________________________");
+            System.out.println("\33[35m__________________________________________________________");
+            System.out.println("\33[39;49m   Las calorías que necesita esta mujer son:  " + calculo );
+            System.out.println("\33[35m__________________________________________________________");
             }
 
     }
